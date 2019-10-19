827faae33a501e178d0fa9d670bfa2c3a486614d
Phumrapee Limpianchop <git@rayriffy.com>
2019-10-19 15:47:45 +0700

    :sparkles: Styled fonts

--- frontend/src/app/components/index.tsx
+++ frontend/src/app/components/index.tsx
@@ -4,11 +4,14 @@ import { Helmet } from 'react-helmet'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import Fonts from './fonts'
+
 import * as Routes from './routes'
 
 const AppComponent: React.FC = () => {
   return (
     <Router>
+      <Fonts />
       <Helmet defaultTitle='YCC slot' titleTemplate='%s · YCC slot' />
       <Switch>
         <Route
